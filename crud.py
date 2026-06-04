e603ca828d51c66412b58120ef4db9e92ba84ca2
Lenovo <2779035040@qq.com>
2026-06-04 10:23:51 +0800

    实现删除学生功能

--- crud.py
+++ crud.py
@@ -15,6 +15,7 @@ def create_student(name, age):
     db.commit()
 
     db.close()
+
   def get_students():
     db = SessionLocal()
 
@@ -28,6 +29,7 @@ def create_student(name, age):
         )
 
     db.close()
+
   def update_student(student_id, name):
     db = SessionLocal()
 
@@ -39,6 +41,21 @@ def create_student(name, age):
 
     if student:
         student.name = name
+        db.commit()
+
+    db.close()
+
+  def delete_student(student_id):
+    db = SessionLocal()
+
+    student = (
+        db.query(Student)
+        .filter(Student.id == student_id)
+        .first()
+    )
+
+    if student:
+        db.delete(student)
         db.commit()
 
     db.close()
